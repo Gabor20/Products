e2c3cc796134994fd4d0201a665008e25538a348
Bodnar Istvan Gabor <gaborbodnar92@gmail.com>
2023-09-14 08:59:38 +0200

    elso

---
+++ PorductAPI/Controllers/product_Controller.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static PorductAPI.DTos;
+
+namespace PorductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class product_Controller : ControllerBase
+    {
+        private static readonly List<ProductDto> products = new()
+        {
+            new ProductDto(Guid.NewGuid(), "Termék1",2500,DateTimeOffset.UtcNow, DateTimeOffset.UtcNow),
+            new ProductDto(Guid.NewGuid(), "Termék2",5500,DateTimeOffset.UtcNow, DateTimeOffset.UtcNow),
+            new ProductDto(Guid.NewGuid(), "Termék3",25000,DateTimeOffset.UtcNow, DateTimeOffset.UtcNow),
+        };
+    }
+}
---
+++ PorductAPI/DTos.cs
@@ -0,0 +1,7 @@
+﻿namespace PorductAPI
+{
+    public class DTos //Data Transfer Object
+    {
+        public record ProductDto(Guid Id, string ProductName, int Productprice, DateTimeOffset CreatedTime, DateTimeOffset ModifiedTime);
+    }
+}
